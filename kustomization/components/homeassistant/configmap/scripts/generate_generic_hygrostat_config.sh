59b111f60e6c2c8f9f66235a999aa0c81fd498fd
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2023-12-24 14:48:18 -0800

    switch swapped logic

--- kustomization/components/homeassistant/configmap/scripts/generate_generic_hygrostat_config.sh
+++ kustomization/components/homeassistant/configmap/scripts/generate_generic_hygrostat_config.sh
@@ -4,14 +4,13 @@ if [ -n "${INCLUDE_GENERIC_HYGROSTAT}" ]; then
     generic_hygrostat_dest=/config/generic_hygrostat.yaml
     
     if [ -f ${generic_hygrostat_dest} ]; then
-        echo "Creating empty ${generic_hygrostat_dest}..."
-    else
         echo "${generic_hygrostat_dest} already exists and is not managed.  Leaving it as-is."
+    else
+        echo "Creating empty ${generic_hygrostat_dest}..."
         echo "---
 []
 " > $generic_hygrostat_dest
         echo "Successfully setup empty configuration at ${generic_hygrostat_dest}"
-
     fi
 
     echo "generic_hygrostat: !include generic_hygrostat.yaml" >> "${config_dest:?}"
