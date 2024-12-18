aea3281338c64df94b69acde474e723ec2c7c89e
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2024-12-18 15:40:12 -0800

    move things to the Dockerfile to prep for caching of the devcontainer

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env sh
 
 set -eu
-
-pip3 install --no-cache-dir -r requirements.txt
-
-ansible-galaxy collection install --upgrade -r requirements.yml
-ansible-galaxy role install --force-with-deps -p external_roles -r requirements.yml
