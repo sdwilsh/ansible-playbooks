00444213dfc5f99d19cc42edca0f99a8f68a212f
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2024-12-18 17:10:18 -0800

    be smarter about the user that the commands run as

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-set -eu
+set -e
+
+ansible-galaxy collection install --upgrade -r requirements.yml
+ansible-galaxy role install --force-with-deps -p external_roles -r requirements.yml
 
 if [ -z "${CI}" ]; then
     ansible-playbook plays/kairos/get_kubeconfig.yml
