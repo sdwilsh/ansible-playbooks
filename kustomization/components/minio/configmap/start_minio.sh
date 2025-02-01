5c71e0a31e9634a173997386d5ad9c96f4203f27
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-02-01 13:17:02 -0800

    Prep work to allow for multiple minio pods in the future.

--- kustomization/components/minio/configmap/start_minio.sh
+++ kustomization/components/minio/configmap/start_minio.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [ -z "${MINIO_CONSOLE_ADDRESS}" ]; then
-    echo "MINIO_CONSOLE_ADDRESS must be set!"
-    exit 1
-fi
-
 if [ -z "${MINIO_ROOT_USER}" ]; then
     echo "MINIO_ROOT_USER must be set!"
     exit 1
@@ -15,4 +10,9 @@ if [ -z "${MINIO_ROOT_PASSWORD}" ]; then
     exit 1
 fi
 
-minio server /data --console-address "${MINIO_CONSOLE_ADDRESS}":9090
+if [ -z "${POD_IP}" ]; then
+    echo "POD_IP must be set!"
+    exit 1
+fi
+
+minio server /data --console-address "${POD_IP}":9090
