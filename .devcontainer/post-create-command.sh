5410e44a303d39f1b88389e67d25ff2772e29244
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-02-06 15:31:19 -0800

    Attempt to fix flaky CI issues

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ansible-galaxy collection install --upgrade -r requirements.yml
+ansible-galaxy collection install --upgrade -p external_collections -r requirements.yml
 ansible-galaxy role install --force-with-deps -p external_roles -r requirements.yml
 
 if [ -z "${CI}" ]; then
