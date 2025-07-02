1c9a947d1f3b528aeece91a530f5d9234c270b3b
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-07-01 20:17:03 -0700

    fix: use auto-generated local credentials

--- kustomization/overlays/prod/crowdsec/configmap/scripts/entrypoint.sh
+++ kustomization/overlays/prod/crowdsec/configmap/scripts/entrypoint.sh
@@ -5,11 +5,6 @@ set -e
 echo "Copying acquis.d configurations"
 rsync -Lav /staging/etc/crowdsec/acquis.d/*.yaml /etc/crowdsec/acquis.d/
 
-echo "Creating /etc/crowdsec/local_api_credentials.yaml"
-echo "url: http://${POD_IP}:3000
-login: localhost
-password: ${LOCAL_API_PASSWORD}" > /etc/crowdsec/local_api_credentials.yaml
-
 echo "Synchronizing default files"
 rsync -av --ignore-existing /staging/etc/crowdsec/* /etc/crowdsec
 
