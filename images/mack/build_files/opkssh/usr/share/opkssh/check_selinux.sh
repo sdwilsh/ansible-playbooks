dcde1353d0b7bb31cb8118292603e42c2e8f7756
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-07-22 20:47:50 -0700

    fix: properly setup selinux for opkssh

--- images/mack/build_files/opkssh/usr/share/opkssh/check_selinux.sh
+++ images/mack/build_files/opkssh/usr/share/opkssh/check_selinux.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+set -x
+
+export OPKSSH_INSTALL_DIR=/usr/bin
+# Fake-out script so it does not run main by default.
+export SHUNIT_RUNNING=1
 # shellcheck source=/dev/null
 source /usr/share/opkssh/install-linux.sh
 
-set -ouex pipefail
+set -oeu pipefail
 
 check_selinux
