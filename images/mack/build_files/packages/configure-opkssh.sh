44adf4e6b391a02e878cef0cfcbd885d1b43f46c
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-07-27 17:05:31 -0700

    fix: include `checkpolicy` when installing opkssh

--- images/mack/build_files/packages/configure-opkssh.sh
+++ images/mack/build_files/packages/configure-opkssh.sh
@@ -14,3 +14,23 @@ chmod -R o= /etc/opk
 # Grab the installer file for the version we are running.  This is used by the
 # opkssh-check-selinux service that is enabled above.
 curl -o /usr/share/opkssh/install-linux.sh https://raw.githubusercontent.com/openpubkey/opkssh/refs/tags/v${OPKSSH_VERSION}/scripts/install-linux.sh 
+
+# Determine what architecture we are building for because some packages do not
+# label with the output from arch.
+TARGETARCHITECTURE=""
+case "${TARGETPLATFORM}" in
+    linux/amd64)
+        TARGETARCHITECTURE="amd64"
+        ;;
+    linux/arm64)
+        TARGETARCHITECTURE="arm64"
+        ;;
+    *)
+        echo "Unable to determine target architecture!"
+        exit 1
+        ;;
+esac
+
+dnf install --setopt=install_weak_deps=False -y \
+    checkpolicy \
+    https://github.com/openpubkey/opkssh/releases/download/v${OPKSSH_VERSION}/opkssh_${OPKSSH_VERSION}_linux_${TARGETARCHITECTURE}.rpm
