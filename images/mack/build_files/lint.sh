40ed59c2effb53f2714a6c76cb67b99344deb6fa
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-07-31 23:48:40 -0700

    fix: run bootc lint on arm64 too

--- images/mack/build_files/lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ouex pipefail
-
-# In GitHub CI for arm64, this fails with a cryptic:
-# error: Linting: Function not implemented (os error 38)
-if [ "${TARGETPLATFORM}" = "linux/amd64" ]; then
-    bootc container lint
-fi
