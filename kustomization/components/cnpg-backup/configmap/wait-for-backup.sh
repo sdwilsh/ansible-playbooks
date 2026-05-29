b6342e3c092f1980639bb296d85cbd5631fb3f69
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2026-05-28 17:22:41 -0700

    fix(cnpg-backup): subshell must be wrapped in quotes

--- kustomization/components/cnpg-backup/configmap/wait-for-backup.sh
+++ kustomization/components/cnpg-backup/configmap/wait-for-backup.sh
@@ -8,7 +8,7 @@ if [ -z "${BACKUP_RESOURCE}" ]; then
     exit 1
 fi
 
-while $(kubectl get backups.postgresql.cnpg.io "${BACKUP_RESOURCE}" -o jsonpath='{.status.phase}') != "complete"
+while "$(kubectl get backups.postgresql.cnpg.io "${BACKUP_RESOURCE}" -o jsonpath='{.status.phase}')" != "complete"
 do
     echo "Sleeping for five seconds since the backup of ${BACKUP_RESOURCE} is not yet complete..."
     sleep 5s
