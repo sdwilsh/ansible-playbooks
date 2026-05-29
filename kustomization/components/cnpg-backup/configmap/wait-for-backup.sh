237d44cf843cf5f49188fe561b29851899d40350
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2026-05-28 19:23:03 -0700

    fix(cnpg-backup): better wait-for-backup script that should actually work

--- kustomization/components/cnpg-backup/configmap/wait-for-backup.sh
+++ kustomization/components/cnpg-backup/configmap/wait-for-backup.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-set -e
-set -x
+set -eoux pipefail
 
 if [ -z "${BACKUP_RESOURCE}" ]; then
     echo "BACKUP_RESOURCE must be set!"
     exit 1
 fi
 
-while "$(kubectl get backups.postgresql.cnpg.io "${BACKUP_RESOURCE}" -o jsonpath='{.status.phase}')" != "complete"
+output=$( \
+    kubectl \
+        get \
+        backups.postgresql.cnpg.io \
+        "${BACKUP_RESOURCE}" \
+        -o jsonpath='{.status.phase}' \
+    ) || exit $?
+while [ "${output}" != "completed" ]
 do
     echo "Sleeping for five seconds since the backup of ${BACKUP_RESOURCE} is not yet complete..."
     sleep 5s
+    output=$( \
+        kubectl \
+            get \
+            backups.postgresql.cnpg.io \
+            "${BACKUP_RESOURCE}" \
+            -o jsonpath='{.status.phase}' \
+        ) || exit $?
 done
+
+echo "Backup of ${BACKUP_RESOURCE} is complete!"
