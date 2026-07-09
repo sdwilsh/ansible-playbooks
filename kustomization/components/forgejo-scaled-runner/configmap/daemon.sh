101f10ac581bf2687f2086baadc755b8a1c3443e
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2026-07-09 13:44:21 -0700

    fix(forgejo-action-runner): create the correct volume type in kata containers

--- kustomization/components/forgejo-scaled-runner/configmap/daemon.sh
+++ kustomization/components/forgejo-scaled-runner/configmap/daemon.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# Setup a real mount for docker when running in kata:
+# https://github.com/kata-containers/kata-containers/blob/main/docs/how-to/how-to-run-docker-with-kata.md
+if [[ $(df -PT /var/lib/docker | awk 'NR==2 {print $2}') == virtiofs ]];
+then
+  truncate -s 40G /mnt/scratch/dind-disk.img
+  mkfs.ext4 -F /mnt/scratch/dind-disk.img
+  mkdir -p /var/lib/docker
+  mount -o loop /mnt/scratch/dind-disk.img /var/lib/docker
+fi
+
 # The original entry point.
 /usr/local/bin/dockerd-entrypoint.sh &
 
